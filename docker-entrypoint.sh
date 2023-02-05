08baaebd7ee185ea45146356884d8f5ea76e8d9c
Andrey Kozyrev <108135223+KozyrevAndrey@users.noreply.github.com>
2023-02-05 21:56:07 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+
+# Collect static files
+echo "Collect static files"
+python3 manage.py collectstatic --noinput
+exec "$@"
+
+# Make migrations
+echo "Make migrations"
+python3 manage.py makemigrations --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python3 manage.py migrate --noinput
+
 
